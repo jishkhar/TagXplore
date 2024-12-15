5885d3a7051c9dc3539e56d650981a5e4e1cc205
jishkhar <jishnukhargharia11@gmail.com>
2024-12-15 23:36:05 +0530

    last -1.2

--- src/App.jsx
+++ src/App.jsx
@@ -26,48 +26,56 @@ function App() {
             Where Time Meets Perspective
           </div>
 
-          <br /><br />
+          <br />
+          <br />
 
-          <div className="text-xl">TEDXSIDDAGANGA INSTITUTE OF TECHNOLOGY Presents:</div>
+          <div className="text-xl">
+            TEDXSIDDAGANGA INSTITUTE OF TECHNOLOGY Presents:
+          </div>
 
           <div className="text-[2.7rem] sm:text-[5rem] flex justify-center mt-5 sm:mt-10">
             <span className="text-red-600">Tag</span>Xplore
           </div>
 
           <div className="text-base sm:text-xl mt-4 sm:m-7 text-justify">
-          
-
-Here’s how it works:  <br />
-
-1️⃣ Find the QR codes in the props placed at AD and GJCB. <br />
-2️⃣ Scan them to uncover the questions.  <br />
-3️⃣ Share a photo of the answer on Instagram and tag us @tedxsiddaganga.   <br /><br />
-
-🎁 Stand a chance to win amazing prizes! <br />
-
-For more details, click here: <a className="text-blue-400" href="https://qr-codes.io/TohuPa">https://qr-codes.io/TohuPa</a>
-<br /><br />
-Get ready to explore, tag, and win! 🌟
+            Here’s how it works: <br /><br />
+            Dive into a journey that blends the essence of time with unique perspectives. <br />
+            <br /> Share a photo of the answer on Instagram and tag us
+            @tedxsiddaganga. <br />
+            <br />
+            🎁 Stand a chance to win amazing prizes! <br />
+            Get ready to explore, tag, and win! 🌟
+            <br /><br />
+            The last date for this event is December 20th.
           </div>
         </main>
 
         <div className="flex flex-col sm:flex-row gap-4 sm:gap-2 items-center sm:ml-36 py-10 px-4">
           <div className="p-8 h-fit sm:h-fit w-full sm:w-[30%] bg-white text-black text-lg">
-            <span className="text-3xl">I</span>n a realm where stones and steel are framed, I stand untouched, yet
-            widely acclaimed. Amidst drafts and plans for earthly design, I hold
-            a flute, with grace divine. Though numbers and angles define this
-            place, I whisper of love, devotion, and grace. Who am I, whose
-            presence so still, Uplifts the heart, bends time to my will?
+            <span className="text-2xl">1. I</span>n a realm where stones and
+            steel are framed, I stand untouched, yet widely acclaimed. Amidst
+            drafts and plans for earthly design, I hold a flute, with grace
+            divine. Though numbers and angles define this place, I whisper of
+            love, devotion, and grace. Who am I, whose presence so still,
+            Uplifts the heart, bends time to my will?
           </div>
           <div className="p-8 h-fit w-full sm:w-[30%] bg-white">
+            <span className="text-2xl text-black">2. </span>
             <img src={im} />
+            <br />
+            <div className="text-black text-xl">
+              Where Am I ??? <br />
+            </div>
+            <div className="text-black text-sm">
+              (Post from a different angle)
+            </div>
           </div>
           <div className="p-8 h-fit sm:h-fit w-full sm:w-[30%] bg-white text-black text-lg">
-          <span className="text-3xl">I</span> carry no books, no desks in sight, Yet I connect with speed and
-            might. Words on paper, parcels in hand, I bridge the gap across the
-            land. A quiet corner, yet bustling with care, Where letters and
-            stamps fill the air. In this college, I stand with pride, What am I,
-            where messages reside?
+            <span className="text-2xl">3. I</span> carry no books, no desks in
+            sight, Yet I connect with speed and might. Words on paper, parcels
+            in hand, I bridge the gap across the land. A quiet corner, yet
+            bustling with care, Where letters and stamps fill the air. In this
+            college, I stand with pride, What am I, where messages reside?
           </div>
         </div>
       </div>
