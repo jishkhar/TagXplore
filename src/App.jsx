843ab90c552af51fb5cbef49849bb13c88fb9cd4
jishkhar <jishnukhargharia11@gmail.com>
2024-12-15 23:38:16 +0530

    last -1.2

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
           <div className="text-base sm:text-xl mt-4 sm:m-7 text-justify">
             
             Dive into a journey that blends the essence of time with unique perspectives. <br />
-            <br />Here’s how it works: <br /><br /> Share a photo of the answer on Instagram and tag us
+            <br />Here’s how it works: <br /><br /> Post a story with the photo of the answer on Instagram and tag us
             @tedxsiddaganga. <br />
             <br />
             🎁 Stand a chance to win amazing prizes! <br />
